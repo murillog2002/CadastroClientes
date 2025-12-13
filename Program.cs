f565ec408e0fa27078c2d837224c87556a013d78
Murillo Gomes <murillog2002@hotmail.com>
2025-12-13 18:38:54 -0300

    feat: estrutura inicial do sistema e menu principal

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool executando = true;
+
+while (executando)
+{
+    Console.Clear();
+    Console.WriteLine("=== SISTEMA DE CADASTRO DE CLIENTES ===");
+    Console.WriteLine("1 - Cadastrar cliente");
+    Console.WriteLine("2 - Listar clientes");
+    Console.WriteLine("3 - Buscar cliente por CPF");
+    Console.WriteLine("0 - Sair");
+    Console.Write("Escolha uma opção: ");
+    
+    string? opcao = Console.ReadLine();
+    
+    switch (opcao)
+    {
+        case "1":
+            Console.WriteLine("Cadastro de cliente (em desenvolvimento)");
+            Console.ReadKey();
+            break;
+        case "2":
+            Console.WriteLine("Listagem de clientes (em desenvolvimento)");
+            Console.ReadKey();
+            break;
+        case "3":
+            Console.WriteLine("Busca de cliente por CPF (em desenvolvimento)");
+            Console.ReadKey();
+            break;
+        case "0":
+            executando = false;
+            Console.WriteLine("Saindo do programa...");
+            Console.ReadKey();
+            break;
+        default:
+            Console.WriteLine("Opção inválida. Tente novamente.");
+            Console.ReadKey();
+            break;
+    }
+}
